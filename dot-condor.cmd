657bab91ecab966a1dc3c1de5d645b8748fa97ec
Oliver Stenbom <oliver.stenbom15@imperial.ac.uk>
2016-12-31 13:47:22 +0000

    Added condor base file

---
+++ dot-condor.cmd
@@ -0,0 +1,39 @@
+#############################
+##
+## Condor job specification for dot to dot
+##
+##############################
+
+universe        = vanilla
+
+executable      = /bin/uname
+
+# This specifies where data sent to STDOUT by the executable should be
+# directed to.
+#
+# The Condor system can perform variable substitution in job specifications;
+# the $(Process) string below will be replaced with the job's Process value.
+# If we submit multiple jobs from this single specification (we do, as you 
+# will see later) then the Process value will be incremented for each job.
+# For example, if we submit 100 jobs, then each job will have a different 
+# Process value, numbered in sequence from 0 to 99.
+#
+# If we were to instruct every job to redirect STDOUT to the same file, then
+# data would be lost as each job would overwrite the same file in an 
+# uncontrolled manner.  Thus, we direct STDOUT for each job to a uniquely 
+# named file.
+output          = out/dot.$(Process).out
+
+# As above, but for STDERR.
+error           = out/dot.$(Process).err
+
+# Condor can write a time-ordered log of events related to this job-set
+# to a file we specify.  This specifies where that file should be written.
+log             = dot-log.log
+
+# This specifies what commandline arguments should be passed to the executable.
+arguments       = -n -m -p
+
+# This specifies that the specification, as parsed up to this point, should be 
+# submitted 5 times.  (If the number is omitted, the number '1' is assumed.)
+queue 5
