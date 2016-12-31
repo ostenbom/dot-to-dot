2f1daf8f1c4f26f4898c7f2d98b1a2a538c587e8
Oliver Stenbom <oliver.stenbom15@imperial.ac.uk>
2016-12-31 13:43:08 +0000

    Started colinearity checking in lineAngle

--- DistanceUtils.py
+++ DistanceUtils.py
@@ -15,13 +15,36 @@ def distanceBetween(first, second):
     return distance
 
 def lineAngle(p1, p2, p3):
-    a = distanceBetween(p1, p2)
-    b = distanceBetween(p2, p3)
-    c = distanceBetween(p1, p3)
-    if a == 0 or b == 0:
+    b1 = distanceBetween(p1, p2)
+    c1 = distanceBetween(p2, p3)
+    a1 = distanceBetween(p1, p3)
+    print ("a: " + str(a1) + " b: " + str(b1) + " c: " + str(c1))
+    print ("p1: " + str(p1) + " p2: " + str(p2) + " p3: " + str(p3))
+    if p1 == (120, 198):
+        import pdb; pdb.set_trace()
+
+
+    areOnLine = colinear(p1, p2, p3)
+
+    if not areOnLine:
+        return 180
+
+    if a1 == 0 or b1 == 0:
         return 90
-    angle = math.degrees(math.acos(float(c * c - a * a - b * b) / float(-2 * a * b)))
+    if abs(a1 - (b1 + c1)) < 0.001:
+        return 180
+    angle = math.degrees(math.acos(float((a1 * a1) - (b1 * b1) - (c1 * c1)) / float(-2 * b1 * c1)))
     return angle
+
+def colinear(p1, p2, p3):
+    x1 = p1[0]
+    x2 = p2[0]
+    x3 = p3[0]
+
+    y1 = p1[0]
+    y2 = p2[0]
+    y3 = p3[0]
+    return (y1 - y2) * (x1 - x3) == (y1 - y3) * (x1 - x2)
 
 def angleBetween(p1, p2):
     assert p1 != p2
