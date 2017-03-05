da85346da105bae8d7eb127550e82c612d767e75
Oliver Stenbom <oliver@stenbom.eu>
2017-03-05 18:09:27 +0000

    OutputImage placing points based on where there is space - increasing dimensions if impossible to place

--- Main.py
+++ Main.py
@@ -44,9 +44,10 @@ lines = timeFunction(traceConverter.getLines)
 print ('Lines to connect: ' + str(len(lines)))
 
 lineConnecter = LineConnecter(lines)
-sortedLines = timeFunction(lineConnecter.getConnectedLines)
+sortedLines = timeFunction(lineConnecter.getConnectedLines, 5)
 
 pointsInOrder = [point for sublist in sortedLines for point in sublist]
 print ('Dots in image: ' + str(len(pointsInOrder)))
-out = OutputImage(pointsInOrder, (width, height), True)
+out = OutputImage(pointsInOrder, (width, height))
 out.showImage()
+out.saveImage()
